ed57e5b42652271c64cc8ecae7b25e738cb9f2f9
Johan Plasencia <plasenciajohan@hotmail.com>
2017-10-31 00:11:54 +0000

    Controles y salto añadidos
    falta hacer retoques

--- DingoWorld/Assets/Scripts/CharacterJump.cs
+++ DingoWorld/Assets/Scripts/CharacterJump.cs
@@ -4,13 +4,76 @@ using UnityEngine;
 
 public class CharacterJump : MonoBehaviour {
 
+
+	private Rigidbody thisRigidbody;
+
+	public float fallMultiplier = 2.5f;
+	public float lowJumpMultiplier = 2f;
+	public float jumpVelocity= 2f;
+	private bool holdingjump;
+	private float GroundCheckDistance = 0.3f;
+
+	
+    public bool grounded =true;
 	// Use this for initialization
 	void Start () {
-		
+		thisRigidbody = gameObject.GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		holdingjump = Input.GetButton("Jump");
 		
+	}
+
+	void FixedUpdate()
+	{
+
+
+
+		checkFirstJump ();
+
+
+	}
+
+
+	void checkFirstJump()
+	{
+		if (holdingjump)
+			thisRigidbody.velocity = Vector3.up * jumpVelocity;
+
+		if (thisRigidbody.velocity.y < 0)
+		{ 
+
+			thisRigidbody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+
+		}
+		else if (thisRigidbody.velocity.y > 0 && !holdingjump)
+		{
+
+			thisRigidbody.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+
+		}
+
+	}
+	
+	 void CheckGroundStatus()
+    {
+        RaycastHit hitInfo;
+#if UNITY_EDITOR
+        // helper to visualise the ground check ray in the scene view
+        Debug.DrawLine(transform.position + (Vector3.up * 0.1f), transform.position + (Vector3.up * 0.1f) + (Vector3.down * GroundCheckDistance));
+#endif
+        // 0.1f is a small offset to start the ray from inside the character
+        // it is also good to note that the transform position in the sample assets is at the base of the character
+        if (Physics.Raycast(transform.position + (Vector3.up * 0.1f), Vector3.down, out hitInfo, GroundCheckDistance))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
 	}
 }
---
+++ DingoWorld/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+   // private const float Y_ANGLE_MIN = 60.0f;
+
+   // private const float Y_ANGLE_MAX = 120.0f;
+    public Transform lookAt;
+
+    public Transform camTransform;
+
+    private Camera cam;
+    private float distance = 10.0f;
+    private float currentX = 0.0f;
+    private float currentY = 0.0f;
+    private float sensivityX = 4.0f;
+    private float sensivityY = 1.0f;
+
+
+     public  float Y_ANGLE_MIN = 0.0f;
+
+     public  float Y_ANGLE_MAX = 120.0f;
+    // Use this for initialization
+    void Start () {
+        camTransform = transform;
+        cam = Camera.main;
+	}
+
+
+    void Update()
+    {
+
+        currentX += Input.GetAxis("Mouse X");
+        currentY += Input.GetAxis("Mouse Y");
+        currentY = Mathf.Clamp(currentY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+    }
+
+    void LateUpdate () {
+
+        Vector3 dir = new Vector3(0, 0 - distance);
+        Quaternion rotation = Quaternion.Euler(currentY, currentX, 0);
+        camTransform.position = lookAt.position + rotation * dir;
+        camTransform.LookAt(lookAt.position);
+        
+    
+	
+	}
+
+
+}
