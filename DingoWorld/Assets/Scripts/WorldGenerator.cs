0c983d81e9075b818871ff74428e3664c6a5d7e5
Johan Plasencia <plasenciajohan@hotmail.com>
2017-10-31 11:40:03 +0000

    Creando estructura para el generador de niveles

--- DingoWorld/Assets/Scripts/WorldGenerator.cs
+++ DingoWorld/Assets/Scripts/WorldGenerator.cs
@@ -4,13 +4,64 @@ using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour {
 
+	public GameObject StartPlat;
+	public GameObject EndPlat;
+
+	public List<GameObject> Size1;
+
+	public List<GameObject> Size2;
+
+	public List<GameObject> Size3;
+
+	public List<GameObject> Size4;
+
+	public int Cols=12;
+	public int Rows=6;
+
+	private int[,] Matrix;
+
+
+	enum PointerDir 
+	{
+		Up,
+		Right,
+		Down,
+		Stop
+	}
+
+
 	// Use this for initialization
 	void Start () {
-		
+
+		Matrix = new int[Rows, Cols];
+		GenerateMatrix ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	private void GenerateMatrix(){
+		int startj=0;
+		int starti = Random.Range (0, Rows - 1);
+		Vector2 pointer= new Vector2(starti,startj);
+
+
+
+		//get dir to go if Stop end.
+
+		//checboundaries
+		//get a random x1 x2 x3 or x4 
+
+
+		//when matrix generated it should be instantiating platforms 
+	}
+
+	private void /*PointerDir*/ DecideDir(Vector2 Pointer){
+	
+		//if( Pointer.y +1 == Cols
+	
+
 	}
 }
