16e2d7ad569993df5cef82b46b5c2ac9045dc43a
Johan Plasencia <plasenciajohan@hotmail.com>
2017-11-05 00:16:52 +0000

    Walljump Creado

--- DingoWorld/Assets/Scripts/PlayerController.cs
+++ DingoWorld/Assets/Scripts/PlayerController.cs
@@ -21,6 +21,11 @@ public CharacterController controller;
 	public float rotateSpeed;
 	public GameObject playerModel;
 	public Animator anim;
+
+	public float walljumpSpeed;
+	public bool isWalljumping;
+	public bool isOnWall;
+	public float wallFallSpeed;
 	// Use this for initialization
 	void Start () {
 		//thisRigidbody= GetComponent<Rigidbody>();
@@ -34,23 +39,35 @@ public CharacterController controller;
 				
 		//moveDirection=new Vector3(Input.GetAxis("Horizontal")*moveSpeed, moveDirection.y,Input.GetAxis("Vertical")*moveSpeed);
 		float yAux=moveDirection.y;
-		moveDirection= (transform.forward *Input.GetAxis("Vertical") )+(transform.right *Input.GetAxis("Horizontal") );
+		if (!isWalljumping)
+			moveDirection= (transform.forward *Input.GetAxis("Vertical") )+(transform.right *Input.GetAxis("Horizontal") );
 		moveDirection=moveDirection.normalized*moveSpeed;
 		moveDirection.y=yAux;
 		if(controller.isGrounded){
 			moveDirection.y=0f;
+			isWalljumping = false;
 			if (Input.GetButtonDown("Jump")){
 				
 				moveDirection.y=jumpForce;
 			}
 
 			anim.SetBool ("Fall", false);
+			//anim.SetBool ("OnWall", false);
+			isOnWall = false;
+			anim.SetBool ("WallJump", false);
 		}
-		
-		moveDirection.y=moveDirection.y +(Physics.gravity.y*gravityScale*Time.deltaTime);
+		moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+		/*if (!isOnWall)
+			moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
+		else {
+				moveDirection.y = moveDirection.y + (Physics.gravity.y * (gravityScale / 8) * Time.deltaTime);
+			if (Mathf.Abs (controller.velocity.y) > wallFallSpeed) {
+				moveDirection.y = (Physics.gravity.y * (gravityScale / 4) * Time.deltaTime);
+			}
+		}*/
 		controller.Move(moveDirection*Time.deltaTime);
 		
-		
+		//Rotate to camera
 		if(Input.GetAxis("Vertical")!=0 || Input.GetAxis("Horizontal")!=0){
 			
 			transform.rotation=Quaternion.Euler(0f,pivot.rotation.eulerAngles.y,0f);
@@ -62,9 +79,41 @@ public CharacterController controller;
 
 		anim.SetBool ("Grounded", controller.isGrounded);
 		anim.SetFloat ("Speed", Mathf.Abs(Input.GetAxis("Vertical"))+  Mathf.Abs(Input.GetAxis("Horizontal")) );
-		
+
+
+
+		anim.SetBool ("OnWall", isOnWall);
 	}
 	
-	
+	private void OnControllerColliderHit (ControllerColliderHit hit)
+	{
+		if(!controller.isGrounded && hit.normal.y < 0.1f)
+		{
+			transform.rotation=Quaternion.Euler(0f,hit.transform.rotation.eulerAngles.y,0f);
+			Quaternion newRotation=Quaternion.LookRotation(new Vector3(moveDirection.x,0f,moveDirection.z));
+			playerModel.transform.rotation=Quaternion.Lerp(playerModel.transform.rotation,newRotation,rotateSpeed *Time.deltaTime);
+			isOnWall=true;
+
+			anim.SetBool ("WallJump", false);
+			if(Input.GetButtonDown("Jump"))
+			{
+				anim.SetBool ("WallJump", true);
+				isOnWall=false;
+				isWalljumping = true;
+				Debug.DrawRay (hit.point, hit.normal, Color.red, 1.25f);
+				moveDirection = hit.normal * walljumpSpeed;
+
+				moveDirection.y=jumpForce;
+
+
+			}
+		}
+
+
+	}
+
+	void OnCollisionExit(Collision collisionInfo) {
+		Debug.Log("No longer in contact with " + collisionInfo.transform.name);
+	}
 
 }
