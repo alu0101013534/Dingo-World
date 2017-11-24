519527c786fb90405e488dc72be4b7c548626bf3
Johan Plasencia <plasenciajohan@hotmail.com>
2017-11-24 12:22:59 +0000

    Creado eescena gameover

---
+++ DingoWorld/Assets/GameOverManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour {
+
+
+	public GameObject gameOverUI;
+
+	public Text scoreText;
+	public float gameOverTimer;
+	private int highScore;
+	private int score;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+		highScore = PlayerPrefs.GetInt ("HighScore",5);
+
+		score = PlayerPrefs.GetInt ("Score",0);
+
+		if (score > highScore) {
+			PlayerPrefs.SetInt ("HighScore", score);
+			scoreText.text = "New HighScore\n" + score.ToString ()+ " Diamonds";
+
+		} 
+		else {
+			scoreText.text = "Score\n" + score.ToString ()+ " Diamonds";
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!gameOverUI.active){
+			gameOverTimer -= Time.deltaTime;
+			if (gameOverTimer < 0) {
+				gameOverUI.SetActive (true);
+
+			}
+		}
+		
+	}
+
+
+	public void NewGame(){
+		
+		Application.LoadLevel (2);
+	}
+
+	public void Menu(){
+	
+
+		Application.LoadLevel (0);
+	}
+}
